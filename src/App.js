000b65fbc35b677ebb0a360035b773faa6c34526
lalitha <lalithatarra147@gmail.com>
2024-02-03 19:37:19 +0530

    added second heading

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
     <h1>This Heading is added by lalitha</h1>
+    <h1>This Heading is added by lalitha in local repository</h1>
     </div>
   );
 }
